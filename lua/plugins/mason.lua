efe0eeb88e483a97286820224005541546d0ffc8
Keiwan Jamaly <33158209+keiwanjamaly@users.noreply.github.com>
2024-11-13 18:15:09 +0100

    Removed some errors at startup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,15 +9,15 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bash-language-server",
-					"clang-format",
-					"cmake-language-server",
-					"haskell-language-server",
-					"jedi-language-server",
-					"lua-language-server",
-					"stylua",
-				},
+				-- ensure_installed = {
+				-- 	"bash-language-server",
+				-- 	"clang-format",
+				-- 	"cmake-language-server",
+				-- 	"haskell-language-server",
+				-- 	"jedi-language-server",
+				-- 	"lua-language-server",
+				-- 	"stylua",
+				-- },
 			})
 		end,
 	},
